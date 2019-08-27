27a75f517f462dfdeaef46804f7db52267f39b64
Asif Ansari <asif.basecamp@gmail.com>
2019-08-27 11:53:52 +0530

    New UI changes

--- src/app/@layout/sidebar/sidebar.component.ts
+++ src/app/@layout/sidebar/sidebar.component.ts
@@ -20,6 +20,7 @@ export class SidebarComponent implements OnInit {
       { "itemName": "Roles", "itemNav": "/main/roles", "itemIcon": "#role", "itemIconSize": "0 0 512.24328 512", "permission":true},
       { "itemName": "Authorization", "itemNav": "/main/authorization", "itemIcon": "#security", "itemIconSize": "-38 0 511 511.99956", "permission":true},
       { "itemName": "Connected Users", "itemNav": "/main/connected-users", "itemIcon": "#connectedUsers", "itemIconSize": "0 0 512.001 512.001", "permission":true},
+      { "itemName": "Tenant", "itemNav": "/main/tenant", "itemIcon": "#tenant", "itemIconSize": "0 0 464 464", "permission":true},
       // { "defaultUrl": "/main/accounts", "itemName": "Accounts", "itemNav": '', "itemModal": false, "itemIcon": "#account", "itemIconSize": "0 0 512 512","permission":true, 
       //             "subMenu":[{"itemName":"All Accounts","itemNav":"/main/accounts", "itemModal": false, "permission":true},
       //                       {"itemName":"Account Life Cycle","itemNav":"/main/accounts/lifecycle", "itemModal": false, "permission":true}]}
--- src/app/authentication/login/login.component.ts
+++ src/app/authentication/login/login.component.ts
@@ -1,22 +1,44 @@
 import { Component, OnInit } from '@angular/core';
 import { NotificationService } from '@progress/kendo-angular-notification';
-
+import { Router } from '@angular/router';    
+import { LoginService } from '../../service/login.service';    
+ import { FormsModule } from '@angular/forms'; 
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.scss']
 })
-export class LoginComponent implements OnInit {
-
-
-  constructor(private notificationService: NotificationService ) { 
-  }
-
-  ngOnInit() {
+export class LoginComponent {    
     
-  }
+  model : any={};    
+    
+  errorMessage:string;    
+  constructor(private router:Router,private LoginService:LoginService,private notificationService: NotificationService ) { }    
+    ngOnInit() {    
+    sessionStorage.removeItem('UserName');    
+    sessionStorage.clear();    
+  }   
 
+  login(){    
+    debugger;    
+    this.LoginService.Login(this.model).subscribe(    
+      data => {    
+        debugger;    
+        //if(data.Status=="Success") 
+        if(data.length>0)   
+        {       
+          this.router.navigate(['/dashboard']);    
+          debugger;    
+        }    
+        else{    
+          this.errorMessage = data.Message;    
+        }    
+      },    
+      error => {    
+        this.errorMessage = error.message;    
+      });    
+  };    
   public show(message: string): void {
     this.notificationService.show({
       content: message,
